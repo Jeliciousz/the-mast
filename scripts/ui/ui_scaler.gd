1ab9657fcc12f5b374536666de30b37ee6f11d8b
Jeliciousz <jeliciousz@proton.me>
2026-07-28 03:11:44 -0400

    these were supposed to be text resources from the beginning

--- scripts/ui/ui_scaler.gd
+++ scripts/ui/ui_scaler.gd
@@ -17,12 +17,12 @@ func _on_setting_changed(setting_name: StringName, new_value: Variant) -> void:
     if setting_name == &"ui_scale":
         match new_value:
             0.75:
-                parent_ui.theme = preload("res://themes/main_0.75x.res")
+                parent_ui.theme = preload("res://themes/main_0.75x.tres")
             1.0:
-                parent_ui.theme = preload("res://themes/main_1x.res")
+                parent_ui.theme = preload("res://themes/main_1x.tres")
             2.0:
-                parent_ui.theme = preload("res://themes/main_2x.res")
+                parent_ui.theme = preload("res://themes/main_2x.tres")
             3.0:
-                parent_ui.theme = preload("res://themes/main_3x.res")
+                parent_ui.theme = preload("res://themes/main_3x.tres")
             4.0:
-                parent_ui.theme = preload("res://themes/main_4x.res")
+                parent_ui.theme = preload("res://themes/main_4x.tres")
