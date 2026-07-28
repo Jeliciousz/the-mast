588d8fb18cc1d03b011a6ec40be3cd2d2621aeda
Jeliciousz <jeliciousz@proton.me>
2026-07-28 18:57:18 -0400

    ui scale needs to be applied on ready

--- scripts/ui/ui_scaler.gd
+++ scripts/ui/ui_scaler.gd
@@ -12,6 +12,18 @@ func _ready() -> void:
 
     Settings.setting_changed.connect(_on_setting_changed)
 
+    match Settings.get_setting(&"ui_scale"):
+        0.75:
+            parent_ui.theme = preload("res://themes/main_0.75x.tres")
+        1.0:
+            parent_ui.theme = preload("res://themes/main_1x.tres")
+        2.0:
+            parent_ui.theme = preload("res://themes/main_2x.tres")
+        3.0:
+            parent_ui.theme = preload("res://themes/main_3x.tres")
+        4.0:
+            parent_ui.theme = preload("res://themes/main_4x.tres")
+
 
 func _on_setting_changed(setting_name: StringName, new_value: Variant) -> void:
     if setting_name == &"ui_scale":
