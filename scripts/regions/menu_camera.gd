8347aade9661172585050a613f49cc884b6f5545
Jeliciousz <jeliciousz@proton.me>
2026-07-31 19:50:33 -0400

    move menu_cam code to separate script

---
+++ scripts/regions/menu_camera.gd
@@ -0,0 +1,42 @@
+extends Camera3D
+
+const BASE_PAN_TO_DURATION: float = 6.0
+const BASE_PAN_FROM_DURATION: float = 4.0
+
+var camera_fly_tween: Tween
+var base_dist_from_player: float
+
+@onready var player: Player = %player
+@onready var menu_view_target: Marker3D = %menu_view_target
+
+
+func _ready() -> void:
+	await owner.ready
+
+	base_dist_from_player = global_position.distance_to(player.head.global_position)
+
+
+func fly_in_tween() -> Tween:
+	if camera_fly_tween:
+		camera_fly_tween.kill()
+
+	var distance = global_position.distance_to(player.head.global_position)
+	var relative_distance = distance / base_dist_from_player
+	var pan_duration = BASE_PAN_TO_DURATION * clampf(relative_distance, 0.0, 1.0)
+	camera_fly_tween = create_tween()
+	camera_fly_tween.tween_property(self, "global_transform", player.head.global_transform, pan_duration).set_trans(Tween.TRANS_CUBIC)
+
+	return camera_fly_tween
+
+
+func fly_out_tween() -> void:
+	global_transform = player.head.global_transform
+
+	if camera_fly_tween:
+		camera_fly_tween.kill()
+
+	var distance = global_position.distance_to(menu_view_target.global_position)
+	var relative_distance = distance / base_dist_from_player
+	var pan_duration = BASE_PAN_FROM_DURATION * clampf(relative_distance, 0.0, 1.0)
+	camera_fly_tween = create_tween()
+	camera_fly_tween.tween_property(self, "global_transform", menu_view_target.global_transform, pan_duration).set_trans(Tween.TRANS_CUBIC)
--- scripts/regions/pod.gd
+++ scripts/regions/pod.gd
@@ -1,15 +1,8 @@
 extends Node3D
 
-const BASE_PAN_TO_DURATION: float = 6.0
-const BASE_PAN_FROM_DURATION: float = 4.0
-
-var camera_fly_tween: Tween
-
 @onready var player: Player = %player
 @onready var pause_menu_ui: PauseMenuUI = $pause_menu_ui
-@onready var menu_view_target: Marker3D = $menu_view_target
 @onready var menu_camera: Camera3D = $menu_camera
-@onready var start_dist_from_player: float = menu_camera.global_position.distance_to(player.head.global_position)
 
 
 func _ready() -> void:
@@ -18,16 +11,7 @@ func _ready() -> void:
 
 
 func _on_play_button_pressed(_event) -> void:
-	if camera_fly_tween:
-		camera_fly_tween.kill()
-
-	var distance = menu_camera.global_position.distance_to(player.head.global_position)
-	var relative_distance = distance / start_dist_from_player
-	var pan_duration = BASE_PAN_TO_DURATION * clampf(relative_distance, 0.0, 1.0)
-	camera_fly_tween = create_tween()
-	camera_fly_tween.tween_property(menu_camera, "global_transform", player.head.global_transform, pan_duration).set_trans(Tween.TRANS_CUBIC)
-
-	await camera_fly_tween.finished
+	await menu_camera.fly_in_tween().finished
 
 	menu_camera.current = false
 	player.camera.current = true
@@ -41,18 +25,10 @@ func _on_play_button_pressed(_event) -> void:
 
 func _on_main_menu_button_pressed(_event) -> void:
 	pause_menu_ui.process_mode = Node.PROCESS_MODE_DISABLED
-	menu_camera.global_transform = player.head.global_transform
 	player.camera.current = false
 	menu_camera.current = true
 
-	if camera_fly_tween:
-		camera_fly_tween.kill()
-
-	var distance = menu_camera.global_position.distance_to(menu_view_target.global_position)
-	var relative_distance = distance / start_dist_from_player
-	var pan_duration = BASE_PAN_FROM_DURATION * clampf(relative_distance, 0.0, 1.0)
-	camera_fly_tween = create_tween()
-	camera_fly_tween.tween_property(menu_camera, "global_transform", menu_view_target.global_transform, pan_duration).set_trans(Tween.TRANS_CUBIC)
+	menu_camera.fly_out_tween()
 
 
 func _on_continue_tag_interacted() -> void:
